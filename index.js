e91d6b18795b0b4740dd49a807a76533ea9c1066
Mohangit03 <mohanpv@codincity.com>
2022-03-11 01:35:19 -0500

    first commit

---
+++ index.js
@@ -0,0 +1,12 @@
+const http = require('http');
+const port = process.env.PORT || 5000;
+
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  const msg = 'iam crickter rohit!\n'
+  res.end(msg);
+});
+
+server.listen(port, () => {
+  console.log(`Server running on http://localhost:${port}/`);
+});
